4186bf63c38e83d0c34648d7f83ddb407b0e29aa
ik012982i9 <ik012982i9@206-03-HZ108356.H012982.NET>
2017-10-11 12:29:41 +0200

    Mas ejercicios con strings

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		String cadena = "Hola Mundo";
+		String cadena1 = "zubiri";
+		String cadena2 = "manteo";
 		
 		System.out.println(cadena);
 		System.out.println("Hola Mundo");
@@ -14,6 +16,31 @@ public class Main {
 		System.out.println(cadena.substring(0, 4));
 		
 		System.out.println(cadena.substring(5, cadena.length()));
+		
+		if(cadena.equals(cadena2)){
+			System.out.println("Son iguales");
+		}
+		else {
+			System.out.println("Son diferentes");
+		}
+		
+		System.out.println(cadena.indexOf("a"));
+		System.out.println(cadena.toUpperCase());
+		System.out.println(cadena.toLowerCase());
+		
+		int comparacion = cadena1.compareTo(cadena2);
+		
+		System.out.println("Comparacion de " + cadena1 + " con " + cadena2 + " es: " + comparacion );
+		
+		if(cadena2.compareTo(cadena1) == 0){
+			System.out.println("son iguales");
+		}
+		
+		
+		// cadena = "Hola mundo";
+		System.out.println("La ultima aparicion de o en " +cadena + " es: " +cadena.lastIndexOf("o"));
+		
+		System.out.println("La primera aparicion de o en " +cadena + " es: " +cadena.indexOf("o"));
 
 	}
 
---
+++ src/MenuStrings.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class MenuStrings {
+
+	public static void main(String[] args) {
+		final int LARGURA = 1;
+		final int ORDENAR = 2;
+		final int EXTRAER = 3;
+		final int SALIR = 4;
+		Scanner scan = new Scanner (System.in);
+		int opcion;
+		do{
+			System.out.println("Elige una de estas opciones:");
+			System.out.println(LARGURA+". Saber el apellido más largo");
+			System.out.println(ORDENAR+". Ordenar alfabeticamente los apellidos");
+			System.out.println(EXTRAER+". Extraer del 'nombre apellido' el apellido");
+			System.out.println(SALIR+". Salir");
+			System.out.println("Pulsa un botón de estos:");
+			
+			opcion = Integer.parseInt(scan.nextLine());
+			switch(opcion){
+				case LARGURA:
+					//escribir 2 apellidos y decir cual es el más largo
+					System.out.println("Escribe un apellido:");
+					String apellido1 = scan.nextLine();
+					System.out.println("Escribe otro apellido:");
+					String apellido2 = scan.nextLine();
+					if (apellido1.length() > apellido2.length()){
+						System.out.println("El apellido más largo es: " + apellido1);
+					}
+					else if (apellido2.length() > apellido1.length()){
+						System.out.println("El apellido más largo es: " + apellido2);
+					}
+					else {
+						System.out.println("Los apellidos son iguales o tienen la misma largura");
+					}
+				break;
+				
+				case ORDENAR:
+					System.out.println("Escribe un apellido:");
+					String apellido3 = scan.nextLine();
+					System.out.println("Escribe otro apellido:");
+					String apellido4 = scan.nextLine();
+					if (apellido3.compareTo(apellido4)> 0){
+						System.out.println("1. " + apellido4);
+						System.out.println("2. " + apellido3);
+					}
+					else if (apellido3.compareTo(apellido4)< 0){
+						System.out.println("1. " + apellido3);
+						System.out.println("2. " + apellido4);
+					}
+					else {
+						System.out.println("Los apellidos son iguales");
+					}
+				break;
+				
+				case EXTRAER:
+					System.out.println("Guardando datos en disco D...");
+				break;
+				
+				case SALIR:
+					System.out.println("Copiando CD...");
+				break;
+				default:
+					System.out.println("Opción incorrecta");
+				
+				
+			
+			}
+		}
+		while(opcion != SALIR);
+			System.out.println("Fin del programa");
+
+	}
+
+}
